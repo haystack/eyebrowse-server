0face04c9534374da3db8d9e7f5d8426bcb6080c
Joshua Blum <joshblum@mit.edu>
2012-10-24 20:51:03 -0400

    static files for gunicorn

--- eyebrowse/urls.py
+++ eyebrowse/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from django.conf import settings
+
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -12,7 +15,9 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('registration.urls')),
 )
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
 
 urlpatterns += patterns('eyebrowse.views', 
     url(r'^$', 'home'),
