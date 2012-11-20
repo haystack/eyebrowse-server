464d5a9fe62fea4a6596a517c5206bff523d5344
Joshua Blum <joshblum@mit.edu>
2012-11-20 15:29:33 -0500

    change profile

--- accounts/models.py
+++ accounts/models.py
@@ -2,18 +2,22 @@ from django.db import models
 from django.utils import simplejson as json
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.conf import settings
+
+from annoying.fields import AutoOneToOneField
 
 from common.admin import email_templates, utils
-from django.conf import settings
 
 from datetime import datetime, timedelta
 
-import sha, random
+import sha
+import random
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     #other fields here
 
+    follows = models.ManyToManyField('UserProfile', related_name='followed_by')
     activation_key = models.CharField(max_length=40, default='')
     pic_url = models.CharField(max_length=1000, default="/static/common/img/placeholder.png")
     use_tour = models.BooleanField(default=True)
