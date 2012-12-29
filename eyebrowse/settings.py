3a55ffb71eefae7d180146bc29862c70124512e0
Joshua Blum <joshblum@mit.edu>
2012-12-29 12:19:53 -0500

    alt logging

--- eyebrowse/settings.py
+++ eyebrowse/settings.py
@@ -35,7 +35,7 @@ DATABASES = {
         'NAME': env['MYSQL_NAME'],# Or path to database file if using sqlite3.
         'USER': env['MYSQL_USER'], # Not used with sqlite3.
         'PASSWORD': env['MYSQL_PASSWORD'],# Not used with sqlite3.
-        'HOST':env['MYSQL_HOST'], # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': env['MYSQL_HOST'], # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '', # Set to empty string for default. Not used with sqlite3.
     }
 }
@@ -197,6 +197,63 @@ TASTYPIE_ALLOW_MISSING_SLASH = True
 #gravatar settings
 GRAVATAR_IMG_CLASS = "img-polaroid"
 
+# # A sample logging configuration. The only tangible logging
+# # performed by this configuration is to send an email to
+# # the site admins on every HTTP 500 error when DEBUG=False.
+# # See http://docs.djangoproject.com/en/dev/topics/logging for
+# # more details on how to customize your logging configuration.
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'simple': {
+#             'format': '%(levelname)s %(asctime)s %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         # Include the default Django email handler for errors
+#         # This is what you'd get without configuring logging at all.
+#         'mail_admins': {
+#             'class': 'django.utils.log.AdminEmailHandler',
+#             'level': 'ERROR',
+#              # But the emails are plain text by default - HTML is nicer
+#             'include_html': True,
+#         },
+#         # Log to a text file that can be rotated by logrotate
+#         'logfile': {
+#             'class': 'logging.handlers.WatchedFileHandler',
+#             'filename': os.path.join(SITE_ROOT, 'logs/log.log'),
+#         },
+#         # Log to a text file that can be rotated by logrotate
+#         'errorfile': {
+#             'class': 'logging.handlers.WatchedFileHandler',
+#             'filename': os.path.join(SITE_ROOT, 'logs/error_log.log'),
+#         },
+#     },
+#     'loggers': {
+#         # Again, default Django configuration to email unhandled exceptions
+#         'django.request': {
+#             'handlers': ['mail_admins'],
+#             'level': 'ERROR',
+#             'propagate': True,
+#         },
+#         # Might as well log any errors anywhere else in Django
+#         'django': {
+#             'handlers': ['errorfile'],
+#             'level': 'ERROR',
+#             'propagate': False,
+#             'formatter': 'simple',
+#         },
+#         # Your own app - this assumes all your logger names start with "myapp."
+#         'eyebrowse.views': {
+#             'handlers': ['logfile'],
+#             'level': 'INFO', # Or maybe INFO or DEBUG
+#             'propagate': False,
+#             'formatter': 'simple',
+#         },
+#     },
+# }
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
@@ -205,53 +262,25 @@ GRAVATAR_IMG_CLASS = "img-polaroid"
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'formatters': {
-        'simple': {
-            'format': '%(levelname)s %(asctime)s %(message)s'
-        },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
     },
     'handlers': {
-        # Include the default Django email handler for errors
-        # This is what you'd get without configuring logging at all.
         'mail_admins': {
-            'class': 'django.utils.log.AdminEmailHandler',
             'level': 'ERROR',
-             # But the emails are plain text by default - HTML is nicer
-            'include_html': True,
-        },
-        # Log to a text file that can be rotated by logrotate
-        'logfile': {
-            'class': 'logging.handlers.WatchedFileHandler',
-            'filename': os.path.join(SITE_ROOT, 'logs/log.log'),
-        },
-        # Log to a text file that can be rotated by logrotate
-        'errorfile': {
-            'class': 'logging.handlers.WatchedFileHandler',
-            'filename': os.path.join(SITE_ROOT, 'logs/error_log.log'),
-        },
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
     },
     'loggers': {
-        # Again, default Django configuration to email unhandled exceptions
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
         },
-        # Might as well log any errors anywhere else in Django
-        'django': {
-            'handlers': ['errorfile'],
-            'level': 'ERROR',
-            'propagate': False,
-            'formatter': 'simple',
-        },
-        # Your own app - this assumes all your logger names start with "myapp."
-        'eyebrowse.views': {
-            'handlers': ['logfile'],
-            'level': 'INFO', # Or maybe INFO or DEBUG
-            'propagate': False,
-            'formatter': 'simple',
-        },
-    },
+    }
 }
 
 if DEBUG:
