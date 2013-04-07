786087c3c3d0df918748bb2348ea6d4ddbb627ed
Joshua Blum <joshblum@mit.edu>
2013-04-06 23:28:39 -0400

    aws and activation email

--- common/helpers.py
+++ common/helpers.py
@@ -25,19 +25,25 @@ def put_profile_pic(url, profile):
         resize_filename, headers = urlretrieve(url + "/resize?w=40&h=40") # store profile sized picture (40x40px)
         conn = S3Connection(settings.AWS["AWS_ACCESS_KEY_ID"], settings.AWS["AWS_SECRET_ACCESS_KEY"])
         b = conn.get_bucket(settings.AWS["BUCKET"])
-        k = Key(b)
 
-        k.key = hashlib.new("md5").update(profile.user.username).hexdigest()
-        k.set_contents_from_filename(filename) 
-        k.set_acl('public-read')
-
-        k = Key(b)
-        k.key = hashlib.new("md5").update(profile.user.username + "resize").hexdigest()
-        k.set_contents_from_filename(resize_filename) 
-        k.set_acl('public-read')
+        _set_key(b, profile.user.username, filename)
+        k = _set_key(b, profile.user.username + "resize", resize_filename)
+        
     except Exception as e:
         print e
         return False
-    #update user profile
+
     return "http://s3.amazonaws.com/%s/%s"% (settings.AWS["BUCKET"], k.key)
+
+def _set_key(b, username, filename):
+    k = Key(b)
+
+    h = hashlib.new("md5")
+    h.update(username)
+    k.key = h.hexdigest()
+    k.set_contents_from_filename(filename) 
+    k.set_acl('public-read')
+
+    return k
+
 
