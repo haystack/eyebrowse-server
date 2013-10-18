686751f12ec637fdaf9d7087c91b047499f573be
Amy Zhang <amy.xian.zhang@gmail.com>
2013-10-18 00:49:42 -0400

    replace deprecated registration urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('accounts.views',
 
-    url(r'', include('registration.urls')),
+    url(r'', include('registration.backends.default.urls')),
     
     url(r'^profile/whitelist$', 'whitelist'),
     url(r'^profile/account$', 'account'),
