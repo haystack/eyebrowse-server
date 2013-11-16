5a36819552907609df6286c5ca6b45a4af89aca2
Amy Zhang <amy.xian.zhang@gmail.com>
2013-11-16 16:24:14 -0500

    add a setup_django file for scripts folder. import this in first line of any script in this folder so it can run from the shell

---
+++ scripts/setup_django.py
@@ -0,0 +1,7 @@
+import os, sys
+
+curdir, curfile = os.path.split(os.path.abspath(__file__))
+parentdir = os.path.normpath(os.path.join(curdir, '..'))
+sys.path.append(parentdir)
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "eyebrowse.settings"
