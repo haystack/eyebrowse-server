d242af1f5e7b5d88daee6397d3d1129e8683ce28
Amy Zhang <amy.xian.zhang@gmail.com>
2013-12-11 22:47:57 -0500

    new table to store raw data - append only

---
+++ api/migrations/0017_copy_eyehistory.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+        # Note: Don't use "from appname.models import ModelName". 
+        # Use orm.ModelName to refer to models in this application,
+        # and orm['appname.ModelName'] for models in other applications.
+        for item in orm.EyeHistory.objects.all():
+            try:
+                dup_raw_histories = orm.EyeHistoryRaw.objects.filter(user=item.user, 
+                                                      url=item.url, 
+                                                      title=item.title, 
+                                                      start_event=item.start_event, 
+                                                      end_event=item.end_event,
+                                                      start_time=item.start_time,
+                                                      end_time=item.end_time)[:1]
+                if len(dup_raw_histories) == 0:
+                    raw = orm.EyeHistoryRaw()
+                    raw.user = item.user
+                    raw.url = item.url
+                    raw.title = item.title
+                    raw.start_event = item.start_event
+                    raw.end_event = item.end_event
+                    raw.start_time = item.start_time
+                    raw.end_time = item.end_time
+                    raw.src = item.src
+                    raw.domain = item.domain
+                    raw.favIconUrl = item.favIconUrl
+                    raw.total_time = item.total_time
+                    raw.humanize_time = item.humanize_time
+                    
+                    raw.save()
+            except:
+                continue
+            
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+    models = {
+        'api.blacklistitem': {
+            'Meta': {'unique_together': "(('user', 'url'),)", 'object_name': 'BlackListItem'},
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 12, 12, 0, 0)'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'api.eyehistory': {
+            'Meta': {'object_name': 'EyeHistory'},
+            'domain': ('django.db.models.fields.URLField', [], {'default': "''", 'max_length': '2000'}),
+            'end_event': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '40'}),
+            'end_time': ('django.db.models.fields.DateTimeField', [], {}),
+            'favIconUrl': ('django.db.models.fields.URLField', [], {'default': "''", 'max_length': '2000'}),
+            'humanize_time': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'src': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '40'}),
+            'start_event': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '40'}),
+            'start_time': ('django.db.models.fields.DateTimeField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2000'}),
+            'total_time': ('django.db.models.fields.IntegerField', [], {}),
+            'url': ('django.db.models.fields.URLField', [], {'default': "''", 'max_length': '2000'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'api.eyehistoryraw': {
+            'Meta': {'object_name': 'EyeHistoryRaw'},
+            'domain': ('django.db.models.fields.URLField', [], {'default': "''", 'max_length': '2000'}),
+            'end_event': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '40'}),
+            'end_time': ('django.db.models.fields.DateTimeField', [], {}),
+            'favIconUrl': ('django.db.models.fields.URLField', [], {'default': "''", 'max_length': '2000'}),
+            'humanize_time': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'src': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '40'}),
+            'start_event': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '40'}),
+            'start_time': ('django.db.models.fields.DateTimeField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '2000'}),
+            'total_time': ('django.db.models.fields.IntegerField', [], {}),
+            'url': ('django.db.models.fields.URLField', [], {'default': "''", 'max_length': '2000'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'api.whitelistitem': {
+            'Meta': {'unique_together': "(('user', 'url'),)", 'object_name': 'WhiteListItem'},
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 12, 12, 0, 0)'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['api']
+    symmetrical = True
--- api/models.py
+++ api/models.py
@@ -26,6 +26,29 @@ class BlackListItem(FilterListItem):
     
     def __unicode__(self):
         return "Blacklist item %s for %s" % (self.url, self.user.username)
+    
+class EyeHistoryRaw(models.Model):
+    user = models.ForeignKey(User)
+
+    src = models.CharField(max_length=40, default='')
+    url = models.URLField(max_length=2000, default='')
+    domain = models.URLField(max_length=2000, default='')
+    favIconUrl = models.URLField(max_length=2000, default='')
+    title = models.CharField(max_length=2000, default='')
+
+    start_event = models.CharField(max_length=40, default='')
+    start_time = models.DateTimeField()
+
+    end_event = models.CharField(max_length=40, default='')
+    end_time = models.DateTimeField()
+
+    total_time = models.IntegerField() # store in ms
+    
+    # store as human readable according to moment.js library: http://momentjs.com/docs/#/displaying/humanize-duration/
+    humanize_time = models.CharField(max_length=200, default='') 
+    
+    def __unicode__(self):
+        return "EyeHistory item %s for %s on %s" % (self.url, self.user.username, self.start_time)
 
 class EyeHistory(models.Model):
     user = models.ForeignKey(User)
