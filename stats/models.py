99c647014c28f6610312834d4d338692f6d4cd82
Amy Zhang <amy.xian.zhang@gmail.com>
2014-03-13 23:33:06 -0400

    make left nav font smaller and make sure favicon exists

--- stats/models.py
+++ stats/models.py
@@ -15,5 +15,13 @@ class FavData(models.Model):
     visit_count = models.IntegerField(blank=True, null=True)
     total_time = models.IntegerField(blank=True, null=True) # store in ms
 
+    def save(self, *args, **kwargs):
+        if self.favIconUrl == '':
+            self.favIconUrl = "http://www.google.com/s2/favicons?domain=%s" % self.domain
+        
+        super(FavData, self).save(*args, **kwargs)
+        
+
+
     def __unicode__(self):
           return "FavData item %s for %s visited %s" % (self.domain, self.user.username, self.visit_count)
