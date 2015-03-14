9daed7a7a611439d1aaac2d3a01317c222a1dc5f
Amy Zhang <amy.xian.zhang@gmail.com>
2015-03-14 00:27:28 -0700

    bug

--- stats/models.py
+++ stats/models.py
@@ -14,27 +14,9 @@ class FavData(models.Model):
     domain = models.URLField(max_length=2000, default='')
     favicon_url = models.TextField(default='')
     favIconUrl = models.URLField(max_length=2000, default='')
-<<<<<<< HEAD
-
-    visit_count = models.IntegerField(blank=True, null=True)
-    total_time = models.IntegerField(blank=True, null=True)  # store in ms
-
-    def save(self, *args, **kwargs):
-        if self.favIconUrl == '':
-            self.favIconUrl = "http://www.google.com/s2/favicons?domain=%s" % self.domain
-
-        super(FavData, self).save(*args, **kwargs)
-
-    def __unicode__(self):
-        return "FavData item %s for %s visited %s" % (
-            self.domain,
-            self.user.username,
-            self.visit_count)
-=======
 
     visit_count = models.IntegerField(blank=True, null=True)
     total_time = models.IntegerField(blank=True, null=True)  # store in ms
 
     def __unicode__(self):
         return "FavData item %s for %s visited %s" % (self.domain, self.user.username, self.visit_count)
->>>>>>> master
