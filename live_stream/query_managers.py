46c56d7a344fd539780997eafaf015cd14e6ace4
Amy Zhang <amy.xian.zhang@gmail.com>
2015-03-13 23:32:58 -0700

    fix bug

--- live_stream/query_managers.py
+++ live_stream/query_managers.py
@@ -283,16 +283,10 @@ class GroupHistory(object):
             tag = Tag.objects.filter(user=req_user, domain=history_item.domain)
             if tag.count() > 0:
                 self.tag = tag[0]
-            
-<<<<<<< HEAD
-        history_item.messages = history_item.eyehistorymessage_set.all()
-        self.history_items = [history_item]
-        self.favicon_url = history_item.favicon_url
-=======
+
         self.history_items = []
         self.add_item(history_item)
         self.favIconUrl = history_item.favIconUrl
->>>>>>> master
         self.user = history_item.user
     
     def add_item(self, history_item):
