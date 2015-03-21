ca83f3784198dadbfe0e132f2cc12fdc0824802b
Amy Zhang <amy.xian.zhang@gmail.com>
2015-03-21 15:25:17 -0400

    add links to twitter following

--- common/constants.py
+++ common/constants.py
@@ -1,8 +1,8 @@
 EMPTY_SEARCH_MSG = {
-    'home_stream': "Broaden your search filters or <a href='/getting_started#startfollowing'>find interesting people to follow</a> to get more results.",
+    'home_stream': "Broaden your search filters or find friends to follow by <a href='/getting_started#startfollowing'>getting some suggestions</a> or <a href='/accounts/profile/sync_twitter'>connecting your Twitter account</a>  to get more results.",
     'self_profile_stream': "You don't have any history yet! <a href='https://chrome.google.com/webstore/detail/eyebrowse/akhbhgcioeledicnkpmjpmkkecohhicf' target='_blank'>Download the extension</a> to start collecting data and <a href='/getting_started'>add sites that you want to track to your whitelist</a>.",
     'profile_stream': """No history data for this user yet, come back soon!""",
-    'self_following': """You aren't following anyone yet! <a href='/getting_started#startfollowing'>Find some interesting people to follow</a>.""",
+    'self_following': """You aren't following anyone yet! <a href='/getting_started#startfollowing'>Get suggestions for people to follow</a> or <a href='/accounts/profile/sync_twitter'>connect your Twitter account</a>.""",
     'following': """This user isn't following anyone just yet.""",
     'followers': """This user doesn't have any followers yet.""",
     'self_followers': """You don't have any followers yet. Invite your friends to try out eyebrowse!""",
