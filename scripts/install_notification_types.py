1ea65fa2dae6d64a9a25ceca334e2fbc33df0baa
Amy Zhang <amy.xian.zhang@gmail.com>
2015-03-21 17:09:38 -0400

    script to add notification types to django

---
+++ scripts/install_notification_types.py
@@ -0,0 +1,12 @@
+import setup_django
+from django.conf import settings
+from django.db.models import signals
+from django.utils.translation import ugettext_noop as _
+
+if "pinax.notifications" in settings.INSTALLED_APPS:
+    from pinax.notifications.models import NoticeType
+    NoticeType.create("new_follower", _("New Follower"), _("You have a new follower"))
+    
+    
+else:
+    print "Skipping creation of NoticeTypes as notification app not found"
