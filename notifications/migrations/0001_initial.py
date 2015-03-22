4f002dab84eab473e512d0cb57617c096d1fad4b
Amy Zhang <amy.xian.zhang@gmail.com>
2015-03-22 16:37:49 -0400

    add notifications to south and add unique

---
+++ notifications/migrations/0001_initial.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'NoticeType'
+        db.create_table('notifications_noticetype', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('label', self.gf('django.db.models.fields.CharField')(max_length=40)),
+            ('display', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('default', self.gf('django.db.models.fields.IntegerField')()),
+        ))
+        db.send_create_signal('notifications', ['NoticeType'])
+
+        # Adding model 'Notification'
+        db.create_table('notifications_notification', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('notice_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['notifications.NoticeType'])),
+            ('seen', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal('notifications', ['Notification'])
+
+        # Adding model 'NoticeSetting'
+        db.create_table('notifications_noticesetting', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('notice_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['notifications.NoticeType'])),
+            ('medium', self.gf('django.db.models.fields.CharField')(max_length=1)),
+            ('send', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('scoping_content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'], null=True, blank=True)),
+            ('scoping_object_id', self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True)),
+        ))
+        db.send_create_signal('notifications', ['NoticeSetting'])
+
+        # Adding unique constraint on 'NoticeSetting', fields ['user', 'notice_type', 'medium', 'scoping_content_type', 'scoping_object_id']
+        db.create_unique('notifications_noticesetting', ['user_id', 'notice_type_id', 'medium', 'scoping_content_type_id', 'scoping_object_id'])
+
+        # Adding model 'NoticeQueueBatch'
+        db.create_table('notifications_noticequeuebatch', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('pickled_data', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal('notifications', ['NoticeQueueBatch'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'NoticeSetting', fields ['user', 'notice_type', 'medium', 'scoping_content_type', 'scoping_object_id']
+        db.delete_unique('notifications_noticesetting', ['user_id', 'notice_type_id', 'medium', 'scoping_content_type_id', 'scoping_object_id'])
+
+        # Deleting model 'NoticeType'
+        db.delete_table('notifications_noticetype')
+
+        # Deleting model 'Notification'
+        db.delete_table('notifications_notification')
+
+        # Deleting model 'NoticeSetting'
+        db.delete_table('notifications_noticesetting')
+
+        # Deleting model 'NoticeQueueBatch'
+        db.delete_table('notifications_noticequeuebatch')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'notifications.noticequeuebatch': {
+            'Meta': {'object_name': 'NoticeQueueBatch'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pickled_data': ('django.db.models.fields.TextField', [], {})
+        },
+        'notifications.noticesetting': {
+            'Meta': {'unique_together': "((u'user', u'notice_type', u'medium', u'scoping_content_type', u'scoping_object_id'),)", 'object_name': 'NoticeSetting'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'medium': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'notice_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['notifications.NoticeType']"}),
+            'scoping_content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
+            'scoping_object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'send': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'notifications.noticetype': {
+            'Meta': {'object_name': 'NoticeType'},
+            'default': ('django.db.models.fields.IntegerField', [], {}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'display': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'max_length': '40'})
+        },
+        'notifications.notification': {
+            'Meta': {'object_name': 'Notification'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'notice_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['notifications.NoticeType']"}),
+            'seen': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['notifications']
