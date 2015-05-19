a641d67c0c456906e05f7e08ef6982add0b37e62
Joshua Blum <joshblum@mit.edu>
2015-05-19 18:49:57 +0300

    admin sites

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 from api.models import BlackListItem
+from api.models import ChatMessage
 from api.models import EyeHistory
+from api.models import EyeHistoryMessage
+from api.models import MuteList
 from api.models import WhiteListItem
 
 admin.site.register(BlackListItem)
+admin.site.register(ChatMessage)
 admin.site.register(EyeHistory)
+admin.site.register(EyeHistoryMessage)
+admin.site.register(MuteList)
 admin.site.register(WhiteListItem)
