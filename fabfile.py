dc97c19bf0a5d98fa891587034c8c6849bc5da93
Joshua Blum <joshblum@mit.edu>
2015-10-16 22:08:52 -0400

    zip first to reduce server downtime

--- fabfile.py
+++ fabfile.py
@@ -13,8 +13,8 @@ def prod():
 
 
 def deploy():
-    sudo('rm -rf %s/*' % env.server_path)
     local('zip -r code.zip * -x "*.pyc" "*.git"')
+    sudo('rm -rf %s/*' % env.server_path)
     put('code.zip', '%s/' % env.server_path, use_sudo=True)
     sudo('cd %s; unzip -o code.zip' % env.server_path)
     sudo('cd %s; rm -f code.zip' % env.server_path)
