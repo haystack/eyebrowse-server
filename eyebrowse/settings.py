5c891bc14b9ba57c7c511c3c9f3034a519501bc4
Amy Zhang <amy.xian.zhang@gmail.com>
2015-10-24 16:08:41 -0400

    more notifications settings

--- eyebrowse/settings.py
+++ eyebrowse/settings.py
@@ -5,6 +5,8 @@ import django
 from registration_defaults.settings import *
 from django.core.urlresolvers import get_resolver
 import languages
+from notifications.backends.email import EmailBackend
+
 
 DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
@@ -234,6 +236,11 @@ PINAX_NOTIFICATIONS_LANGUAGE_MODEL = "languages.Language"
 
 def PINAX_NOTIFICATIONS_GET_LANGUAGE_MODEL():
     return languages.models.Language
+
+PINAX_NOTIFICATIONS_BACKENDS = [('email', EmailBackend)]
+
+PINAX_NOTIFICATIONS_QUEUE_ALL = False
+
 
 # email settings from sendgrid.com
 EMAIL_HOST = EMAIL["EMAIL_HOST"]
