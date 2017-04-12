78b49c66c3c3e1232a3e8f860145431bf9e91e8b
Jessica <jessizici@gmail.com>
2017-04-12 17:14:52 -0400

    Tags and pages controller functions

--- tags/urls.py
+++ tags/urls.py
@@ -1,4 +1,9 @@
 from django.conf.urls import patterns
 from django.conf.urls import url
 
-urlpatterns = patterns('tags.views')
+urlpatterns = patterns('tags.views',
+                        url(r'^value_tag$', 'value_tag'),
+                        url(r'^tags/page', 'tags_by_page'),
+                        url(r'^page$', 'page'),
+                        url(r'^initialize_page$', 'initialize_page'),
+                      )
