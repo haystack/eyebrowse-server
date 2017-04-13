f447239887d62206557026dd0532bb0552613c65
Jessica <jessizici@gmail.com>
2017-04-13 02:15:25 -0400

    Added highlighting controllers

--- tags/urls.py
+++ tags/urls.py
@@ -4,8 +4,11 @@ from django.conf.urls import url
 urlpatterns = patterns('tags.views',
                         url(r'^value_tag$', 'value_tag'),
                         url(r'^tags/page', 'tags_by_page'),
+                        url(r'^tags/highlight', 'tags_by_highlight'),
                         url(r'^page$', 'page'),
                         url(r'^initialize_page$', 'initialize_page'),
                         url(r'^vote/add$', 'add_vote'),
                         url(r'^vote/remove$', 'remove_vote'),
+                        url(r'^highlight$', 'highlight'),
+                        url(r'^highlights$', 'highlights'),
                       )
