a4823a48a118b6acea96ff6d3ef93b49c25e2073
PrateekKuk <prateek.kukreja@gmail.com>
2018-05-15 00:44:49 -0400

    updated urls to match new endpoints

--- tags/urls.py
+++ tags/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url
 
 urlpatterns = patterns('tags.views',
                         url(r'^tags/page', 'tags_by_page'),
-                        url(r'^tags/highlight', 'tags_by_highlight'),
+                        url(r'^tags/highlight', 'highlight_by_id'),
                         url(r'^tags/comment', 'tags_by_comment'),
                         url(r'^page$', 'page'),
                         url(r'^page/comments$', 'comments_by_page'),
-                        url(r'^initialize_page$', 'initialize_page'),
+                        url(r'^initialize_page$', 'initialize_page_highlights'),
                         url(r'^vote/add$', 'add_vote'),
                         url(r'^vote/remove$', 'remove_vote'),
                         url(r'^highlight$', 'highlight'),
